3376ec88441fdd2bd6674138365bc4ec8f20a33a
lkt345 <lkt345@gmail.com>
2012-11-15 08:35:50 -0600

    huella-ecologica final

--- huella-ecologica.py
+++ huella-ecologica.py
@@ -1,60 +1,94 @@
 #!/usr/bin/python3
 # encoding: UTF-8
-# Diegomf
+# Sauλ de Nova and Diegomf
 
-# Programa de Cambio Climático
+import pickle
+import threading
 
-## LETS DO THIS SHIT!
+stdinput = threading.Lock()
+questionsResults = [False for i in range(8)]
 
-########################################
-# funcion mainMenu
-# regresar lista con valores de t/f (8)
-########################################
+class getResultsThread(threading.Thread):
+	def __init__(self, question, questionNumber):
+		self.question = question
+		self.questionNumber = questionNumber
+		threading.Thread.__init__(self)
+	
+	def run(self):
+		global questionsResults
+		with stdinput:
+			result = exeQuestions(self.question)
+		questionsResults[self.questionNumber] = result
 
 def mainMenu():
-	
+	"""
+	Interface that interacts with the user and asks him for his data.
+	"""
 	print("¡Bienvenido a nuestro programa para calcular tu Huella Ecológica!")
-	print("En este programa sabrás tu Huella Ecológica y la prodrás comparar con la de otras personas en el mundo")
+	print("En este programa sabrás tu Huella Ecológica y la podrás comparar con la de otras personas en el mundo")
 
 	initialize = str(input("Teclea ENTER para empezar")) 
 	
 	f_questions = open("Questions.txt", "r")
 	questions = f_questions.readlines()
 	f_questions.close()
-	answers = []
 
-	for line in questions:
-		answers.append(exeQuestions(line))	
+	threadsQuestions = []
+	for i in range(len(questions)):
+		threadQuestions = getResultsThread(questions[i], i)
+		threadQuestions.start()
+		threadsQuestions.append(threadQuestions)
 	
-	number = process(answers)
-	print("Tu Huella Ecológica es: %i " % (number))
+	for thread in threadsQuestions:
+		thread.join()
+	
+	value = process(questionsResults)
 
-def exeQuestions(question):
-	notAccepted = True
-	while notAccepted:
+	try:
+		globalResults = pickle.load( open("resultados.txt", "rb") )
+	except EOFError as e:
+		globalResults = [value]
+	else:
+		globalResults.append(value)
+	pickle.dump(globalResults, open("resultados.txt", "wb") )
+
+	print("Tu Huella Ecológica es: %i. El promedio de las demás personas es: %i" % (value, sum(globalResults)/len(globalResults)))
+
+def exeQuestions(question, valueTrue=1, valueFalse=2):
+	"""
+	Generic template for obtaining results from the user, 
+	Gets from the user either a valueTrue, valueFalse
+	Returns a boolean, 1->True, 2->False
+	"""
+	accepted = False
+	while not accepted:
 		try:
 			a = int(input(question))
 		except ValueError:
 			print("Valor no válido, intenta de nuevo")
 			continue
-		if a == 1 :
-			return True
-		elif a == 2:
-			return False
-		elif a == 999:
+		if a == valueTrue or a == valueFalse:
+			returnVal = True
+			if a == valueFalse: # If a is equal to false, then returnVal is false, else true
+				returnVal = False
+			accepted = True
+		elif a == 999: # Easter egg
 			print("Lol faggot!!")
-			a
 		else:
 			print("Valor no válido, intenta de nuevo")
+	return returnVal
 
 def process(dataList):
+	"""
+	Receives the list of the values obtained from the questions and from the
+	value of the valoresHuella.txt file and calculates the result of the exam
+	returns the sum of the values
+	"""
 	archivoValores = open("valoresHuella.txt", "r")
-	values = [archivoValores.readline().split(" ") for i in range(len(dataList))]
+	sumValues = sum([int(archivoValores.readline().split(" ")[0 if dataList[i] else 1]) for i in range(len(dataList))])
 	archivoValores.close()
-	sumValues = 0
-	for i in range(len(dataList)):
-		sumValues += int(values[i][0 if dataList[i] else 1])
 	return sumValues 
 
 if __name__ == "__main__":
 	mainMenu()
+
